2025fc80484418fac3d569636aba9769f24c075b
srivijayram <srivijayram003@gmail.com>
2021-08-30 15:10:44 +0530

    Update even_numbers.c

--- even_numbers.c
+++ even_numbers.c
@@ -5,7 +5,7 @@ int main()
     int n,i;
     printf("Enter the number to generate even numbers until n:");
     scanf("%d",&n);
-    for(i=0;i<n;i=i+2){
+    for(i=0;i<=n;i=i+2){
         printf("\n%d",i);
     }
     return 0;
